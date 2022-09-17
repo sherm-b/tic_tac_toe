8c0320f8186cc1c5582089faf48844ca7655941c
jeremy-b <jeremy.steven.bess@gmail.com>
2022-09-16 20:42:24 -0700

    Lint file

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,30 +1,32 @@
 class Board
-  def initialize(p1_name="Player one", p2_name="Player two")
-    @row_a = [" "," "," "]
-    @row_b = [" "," "," "]
-    @row_c = [" "," "," "]
+  def initialize(p1_name = 'Player one', p2_name = 'Player two')
+    @row_a = [' ', ' ', ' ']
+    @row_b = [' ', ' ', ' ']
+    @row_c = [' ', ' ', ' ']
     @p1_name = p1_name
     @p2_name = p2_name
   end
+
   def show_board
-    puts "  1   2   3 "
+    puts '  1   2   3 '
     puts "A #{@row_a[0]} | #{@row_a[1]} | #{@row_a[2]} "
-    puts " - - - - - -"
+    puts ' - - - - - -'
     puts "B #{@row_b[0]} | #{@row_b[1]} | #{@row_b[2]} "
-    puts " - - - - - -"
+    puts ' - - - - - -'
     puts "C #{@row_c[0]} | #{@row_c[1]} | #{@row_c[2]} "
   end
+
   def make_move
-    puts "Make your move! Input the row and column of the cell you want to play."
-    move_ary = gets.chomp.downcase.split("")
-    if move_ary[0] == "a" && @row_a[move_ary[1].to_i - 1] == " "
+    puts 'Make your move! Input the row and column of the cell you want to play.'
+    move_ary = gets.chomp.downcase.split('')
+    if move_ary[0] == 'a' && @row_a[move_ary[1].to_i - 1] == ' '
       @row_a[move_ary[1].to_i - 1] = 'X'
-    elsif move_ary[0] == "b" && @row_b[move_ary[1].to_i - 1] == " "
+    elsif move_ary[0] == 'b' && @row_b[move_ary[1].to_i - 1] == ' '
       @row_b[move_ary[1].to_i - 1] = 'X'
-    elsif move_ary[0] == "c" && @row_c[move_ary[1].to_i - 1] == " "
+    elsif move_ary[0] == 'c' && @row_c[move_ary[1].to_i - 1] == ' '
       @row_c[move_ary[1].to_i - 1] = 'X'
     else
-      puts "Invalid Move!"
+      puts 'Invalid Move!'
     end
     show_board
   end
