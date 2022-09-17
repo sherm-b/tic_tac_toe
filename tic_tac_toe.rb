64d5075128cf243c2067bb38cf1713466d2cf8a9
jeremy-b <jeremy.steven.bess@gmail.com>
2022-09-17 13:31:29 -0700

    Create method to check for a winner and add parameter to round method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,11 @@
 class Board
   def initialize(p1_name = 'Player one', p2_name = 'Player two')
-    @rows = {row_a: [' ',' ',' '], row_b: [' ',' ',' '], row_c: [' ',' ',' ']}
     @p1_name = p1_name
     @p2_name = p2_name
   end
+  @rows = {row_a: [' ',' ',' '], row_b: [' ',' ',' '], row_c: [' ',' ',' ']}
+  @p1_token = 'X'
+  @p2_token = 'O'
 
   def show_board
     puts '  1   2   3 '
@@ -14,22 +16,37 @@ class Board
     puts "C #{@rows[:row_c][0]} | #{@rows[:row_c][1]} | #{@rows[:row_c][2]} "
   end
 
-  def make_move
+  def make_move(token)
     puts 'Make your move! Input the row and column of the cell you want to play.'
     move_ary = gets.chomp.downcase.split('')
     if move_ary[0] == 'a' && @rows[:row_a][move_ary[1].to_i - 1] == ' '
-      @rows[:row_a][move_ary[1].to_i - 1] = 'X'
+      @rows[:row_a][move_ary[1].to_i - 1] = "#{token}"
     elsif move_ary[0] == 'b' && @rows[:row_b][move_ary[1].to_i - 1] == ' '
-      @rows[:row_b][move_ary[1].to_i - 1] = 'X'
+      @rows[:row_b][move_ary[1].to_i - 1] = "#{token}"
     elsif move_ary[0] == 'c' && @rows[:row_c][move_ary[1].to_i - 1] == ' '
-      @rows[:row_c][move_ary[1].to_i - 1] = 'X'
+      @rows[:row_c][move_ary[1].to_i - 1] = "#{token}"
     else
       puts 'Invalid Move!'
     end
     show_board
   end
+
+  def winner?
+    if @rows.each do {|row_name, row| row.all? {|cell| cell == 'X' || cell == 'O'}}
+      return true
+    elsif @rows[:row_a][0] != " " && @rows[:row_a][0] == @rows[:row_b][1] && @rows[:row_b][1] == @rows[:row_c][2]
+      return true
+    elsif @rows[:row_a][2] != " " && @rows[:row_a][2] == @rows[:row_b][1] && @rows[:row_b][1] == @rows[:row_c][0]
+      return true
+    end
+    i = 0
+    while i > 3
+      if @rows[:row_a][i] == @rows[:row_b][i] && @rows[:row_b][i] == @rows[:row_c][i]
+        return true
+      end
+    end
+    return false
+  end
+
 end
 
-board = Board.new
-board.show_board
-board.make_move
