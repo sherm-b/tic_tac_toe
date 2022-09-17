3b925b0b21d49abb68f488da28887dbaefc2548e
jeremy-b <jeremy.steven.bess@gmail.com>
2022-09-16 19:24:32 -0700

    Begin board class and create function to draw board

---
+++ tic_tac_toe.rb
@@ -0,0 +1,16 @@
+class Board
+  def initialize(p1_name="Player one", p2_name="Player two")
+    @row_a = ["","",""]
+    @row_b = ["","",""]
+    @row_c = ["","",""]
+    @p1_name = p1_name
+    @p2_name = p2_name
+  end
+  def show_board
+    puts "#{row_a[0]} | #{row_a[1]} | #{row_a[2]}"
+    puts "- - - - - -"
+    puts "#{row_b[0]} | #{row_b[1]} | #{row_b[2]}"
+    puts "- - - - - -"
+    puts "#{row_c[0]} | #{row_c[1]} | #{row_c[2]}"
+  end
+end
