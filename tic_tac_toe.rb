7cd20673f78c010f0314a3194f91bf76f70fc62d
jeremy-b <jeremy.steven.bess@gmail.com>
2022-09-17 15:38:04 -0700

    Create game loop, check for cat's game, and condition to retry move if invalid

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,10 +2,10 @@ class Board
   def initialize(p1_name = 'Player one', p2_name = 'Player two')
     @p1_name = p1_name
     @p2_name = p2_name
+    @rows = {row_a: [' ',' ',' '], row_b: [' ',' ',' '], row_c: [' ',' ',' ']}
+    @p1_token = 'X'
+    @p2_token = 'O'
   end
-  @rows = {row_a: [' ',' ',' '], row_b: [' ',' ',' '], row_c: [' ',' ',' ']}
-  @p1_token = 'X'
-  @p2_token = 'O'
 
   def show_board
     puts '  1   2   3 '
@@ -17,22 +17,28 @@ class Board
   end
 
   def make_move(token)
-    puts 'Make your move! Input the row and column of the cell you want to play.'
-    move_ary = gets.chomp.downcase.split('')
-    if move_ary[0] == 'a' && @rows[:row_a][move_ary[1].to_i - 1] == ' '
-      @rows[:row_a][move_ary[1].to_i - 1] = "#{token}"
-    elsif move_ary[0] == 'b' && @rows[:row_b][move_ary[1].to_i - 1] == ' '
-      @rows[:row_b][move_ary[1].to_i - 1] = "#{token}"
-    elsif move_ary[0] == 'c' && @rows[:row_c][move_ary[1].to_i - 1] == ' '
-      @rows[:row_c][move_ary[1].to_i - 1] = "#{token}"
-    else
-      puts 'Invalid Move!'
+    invalid_move = true
+    until invalid_move == false
+      puts 'Make your move! Input the row and column of the cell you want to play.'
+      move_ary = gets.chomp.downcase.split('')
+      if move_ary[0] == 'a' && @rows[:row_a][move_ary[1].to_i - 1] == ' '
+        @rows[:row_a][move_ary[1].to_i - 1] = "#{token}"
+        invalid_move = false
+      elsif move_ary[0] == 'b' && @rows[:row_b][move_ary[1].to_i - 1] == ' '
+        @rows[:row_b][move_ary[1].to_i - 1] = "#{token}"
+        invalid_move = false
+      elsif move_ary[0] == 'c' && @rows[:row_c][move_ary[1].to_i - 1] == ' '
+        @rows[:row_c][move_ary[1].to_i - 1] = "#{token}"
+        invalid_move = false
+      else
+        puts 'Invalid Move! Try again'
+      end
     end
     show_board
   end
 
   def winner?
-    if @rows.each do {|row_name, row| row.all? {|cell| cell == 'X' || cell == 'O'}}
+    if @rows.any? {|row_name, row| row[0] != " " && row[0] == row[1] && row[1] == row[2]}
       return true
     elsif @rows[:row_a][0] != " " && @rows[:row_a][0] == @rows[:row_b][1] && @rows[:row_b][1] == @rows[:row_c][2]
       return true
@@ -40,13 +46,59 @@ class Board
       return true
     end
     i = 0
-    while i > 3
-      if @rows[:row_a][i] == @rows[:row_b][i] && @rows[:row_b][i] == @rows[:row_c][i]
+    while i < 3
+      if @rows[:row_a][i] != " " && @rows[:row_a][i] == @rows[:row_b][i] && @rows[:row_b][i] == @rows[:row_c][i]
         return true
       end
+      i += 1
     end
     return false
   end
 
+  def cats_game?
+    cats_check = 0
+    @rows.each do |row_name, row|
+      if row.none? {|cell| cell == " "}
+        cats_check += 1
+      end
+    end
+    if cats_check == 3
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_loop
+    show_board
+    loop do
+      puts "#{@p1_name}'s Turn"
+      make_move(@p1_token)
+      if winner?
+        puts "#{@p1_name} Wins!"
+        break
+      elsif cats_game?
+        puts "Cat's game! nobody wins!"
+        break
+      end
+      puts "#{@p2_name}'s Turn"
+      make_move(@p2_token)
+      if winner?
+        puts "#{@p2_name} wins!"
+        break
+      elsif cats_game?
+        puts "Cat's game! nobody wins!"
+        break
+      end
+    end
+  end
+
 end
 
+puts "Player 1: enter your name"
+p1 = gets.chomp
+puts "Player 2: enter your name"
+p2 = gets.chomp
+
+board = Board.new(p1, p2)
+board.game_loop
