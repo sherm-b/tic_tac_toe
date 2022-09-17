ff7c1f2e3dcf37a7391366879b89555455cc2daf
jeremy-b <jeremy.steven.bess@gmail.com>
2022-09-16 19:35:36 -0700

    Create legend for row and column positions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,10 +7,11 @@ class Board
     @p2_name = p2_name
   end
   def show_board
-    puts " #{@row_a[0]} | #{@row_a[1]} | #{@row_a[2]} "
-    puts "- - - - - -"
-    puts " #{@row_b[0]} | #{@row_b[1]} | #{@row_b[2]} "
-    puts "- - - - - -"
-    puts " #{@row_c[0]} | #{@row_c[1]} | #{@row_c[2]} "
+    puts "  1   2   3 "
+    puts "A #{@row_a[0]} | #{@row_a[1]} | #{@row_a[2]} "
+    puts " - - - - - -"
+    puts "B #{@row_b[0]} | #{@row_b[1]} | #{@row_b[2]} "
+    puts " - - - - - -"
+    puts "C #{@row_c[0]} | #{@row_c[1]} | #{@row_c[2]} "
   end
 end
